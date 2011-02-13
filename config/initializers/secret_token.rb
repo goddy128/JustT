f625db3e016f4026135d9cf3327b904ec2e7a801
goddy128 <goddy128@gmail.com>
2011-02-13 16:37:46 +0800

    init project
    Signed-off-by: goddy128 <goddy128@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Justt::Application.config.secret_token = 'f994ff40c42f08e55a8a7e82448674b672bbf92002cd79a28e0768472b2fabe00fd862f3e8165a8f6a141d01f0cd6a461ebdfcfa7499a8be8b4d987f3a7ca00b'
